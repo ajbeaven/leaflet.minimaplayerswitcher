3340ea2b608695e8543cf998ea9991ac5487bd5d
jacobtoye <jacob.toye@gmail.com>
2013-08-27 11:12:57 +1200

    Added build and lint tasks.

---
+++ build/deps.js
@@ -0,0 +1,13 @@
+var deps = {
+	MiniMapLayerSwitcher: {
+		src: [
+			'Leaflet.minimaplayerswitcher.js',
+			'Control.MiniMapLayerSwitcher.js'
+		],
+		desc: 'The plugin code.'
+	}
+};
+
+if (typeof exports !== 'undefined') {
+	exports.deps = deps;
+}
